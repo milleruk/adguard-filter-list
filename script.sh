d2ab99b8d5c87f19997d62896a4c98a6bdbb2d9b
root <root@agh.localdomain>
2022-06-05 19:26:43 +0000

    Blocklist update 2022-06-05 19:26:43

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 cd /home/adguard-filter-list
 
 # Create compiled blocklist
-time /usr/local/bin/hostlist-compiler -v -c hostlist-compiler-config.json -o blocklist
+#time /usr/local/bin/hostlist-compiler -v -c hostlist-compiler-config.json -o blocklist
 
 # Date and time
 currentDate=`/bin/date '+%Y-%m-%d'`
 currentTime=`/bin/date '+%H:%M:%S'`
 
 # Push it to GitHub
-#git add *
-#git commit -m "Blocklist update ${currentDate} ${currentTime}"
-#git push
+git add *
+git commit -m "Blocklist update ${currentDate} ${currentTime}"
+git push
